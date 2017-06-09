fb64da820820f04004ef932cc82837b563673279
connectrazz <connectme.razz@live.com>
2017-06-09 18:29:50 +0700

    Create CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -106,7 +106,8 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1
 
 //TODO Add here your network seed nodes
 const std::initializer_list<const char*> SEED_NODES = {
-  "159.203.143.171:27010",
+  "45.76.154.131:47094",
+  "45.76.179.189:47094"
   //"your_seed_ip2.com:8080",
 };
 
